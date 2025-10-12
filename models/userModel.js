2307a2143ba4b24524466a986b91ea49e339b883
rhona-tsc <rhona@thesupremecollective.co.uk>
2025-10-12 18:43:47 +0100

    removed lastname as reqd

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
+    lastName: {type: String, required: false},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     phone: {type: String, required: true},
