ade307da2564097b09cbcfe96ec04a24e2d317e6
rhona-tsc <rhona@thesupremecollective.co.uk>
2025-10-13 18:41:22 +0100

    shortlist model correction

--- models/shortlistModel.js
+++ models/shortlistModel.js
@@ -1,16 +1,14 @@
 // models/shortlistModel.js
 import mongoose from "mongoose";
 
-const ShortlistSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  acts: [
-    {
+const shortlistSchema = new mongoose.Schema(
+   {
       actId: { type: mongoose.Schema.Types.ObjectId, ref: "Act" },
       dateISO: { type: String },
       formattedAddress: { type: String }, // new field for venue/location
     },
-  ],
-});
+  { timestamps: true }
+);
 
 const Shortlist = mongoose.model("Shortlist", shortlistSchema);
 export default Shortlist;
