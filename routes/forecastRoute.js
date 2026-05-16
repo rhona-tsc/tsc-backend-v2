92c57b238b7275876d1c821757bb06ac7c97c65b
rhona-tsc <rhona@thesupremecollective.co.uk>
2026-05-16 20:22:05 +0100

    monthly forecast route

--- routes/forecastRoute.js
+++ routes/forecastRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getForecastTimeline } from "../controllers/forecastController.js";
-
+import {
+  getForecastTimeline,
+  getForecastMonthlySummary,
+} from "../controllers/forecastController.js";
 const forecastRouter = express.Router();
 
+forecastRouter.get("/monthly-summary", getForecastMonthlySummary);
 forecastRouter.get("/timeline", getForecastTimeline);
 
 export default forecastRouter;
