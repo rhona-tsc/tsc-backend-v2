35237b5e5a1402df40697269369dde5ff26b7cfa
rhona-tsc <rhona@thesupremecollective.co.uk>
2026-05-17 20:58:23 +0100

    adding tax and vat to forecast events

--- routes/financeTaxRoute.js
+++ routes/financeTaxRoute.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getVatForecast,
   getCorporationTaxForecast,
+  generateTaxForecastEvents,
 } from "../controllers/financeTaxController.js";
 
 const financeTaxRouter = express.Router();
 
 financeTaxRouter.get("/vat-forecast", getVatForecast);
 financeTaxRouter.get("/corporation-tax-forecast", getCorporationTaxForecast);
+financeTaxRouter.post("/generate-forecast-events", generateTaxForecastEvents);
 
 export default financeTaxRouter;
