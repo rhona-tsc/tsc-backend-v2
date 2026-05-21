b82c8916bd9e02f903b475a884eb78cbfbfd8b80
rhona-tsc <rhona@thesupremecollective.co.uk>
2026-05-21 19:30:52 +0100

    update

--- routes/forecastRoute.js
+++ routes/forecastRoute.js
@@ -2,10 +2,12 @@ import express from "express";
 import {
   getForecastTimeline,
   getForecastMonthlySummary,
+  syncBookingFromBoard,
 } from "../controllers/forecastController.js";
 const forecastRouter = express.Router();
 
 forecastRouter.get("/monthly-summary", getForecastMonthlySummary);
 forecastRouter.get("/timeline", getForecastTimeline);
+forecastRouter.post("/bookings/sync-from-board/:bookingId", syncBookingFromBoard);
 
 export default forecastRouter;
